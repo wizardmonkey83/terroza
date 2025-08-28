ddf5e3ae1c00dbb171de0251d41adcbdb00f2985
Benjamin <benjamin630@icloud.com>
2025-08-28 13:06:51 -0700

    this is finna be a hard one, no diddy. began adding account models. went over webpage structure

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# user profile
+class UserProfile(models.Model):
+    username = models.CharField(max_length=30)
+    date_created = models.DateField()
--- project/books/apps.py
+++ project/books/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyAppConfig(AppConfig):
+class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    name = 'books'
