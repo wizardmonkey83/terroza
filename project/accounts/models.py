3261f2348fc9b11a75b62617e0e81b911547fa63
Benjamin <benjamin630@icloud.com>
2025-09-01 17:33:09 -0700

    added userstats and userprofile models

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -4,7 +4,22 @@ from django.db import models
 
 # user profile
 class UserProfile(models.Model):
-    username = models.CharField(max_length=30)
+    # make sure to include error statements for these in views
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
     date_created = models.DateField()
     image = models.ImageField(upload_to='profile/')
-    
+
+class UserStats(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    total_points = models.IntegerField()
+    rank = models.IntegerField()
+    books_read = models.IntegerField()
+    chapters_read = models.IntegerField()
+    words_written = models.IntegerField()
+
+class UserEntries(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    entry = models.TextField()
+    book = models.ForeignKey
+
