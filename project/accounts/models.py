207f63991cb3c8032e59ecebea37ecdd9fa1217a
Benjamin <benjamin630@icloud.com>
2025-09-03 13:08:04 -0700

    finished three core models for mvp: user, book, readinglog. moving onto next steps

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,25 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 # user profile
-class UserProfile(models.Model):
-    # make sure to include error statements for these in views
-    username = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(unique=True)
-    date_created = models.DateField()
-    image = models.ImageField(upload_to='profile/')
+class Profile(models.Model):
+    # OneToOneField links the user to each instance of the model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = models.PositiveIntegerField(default=0)
+    books_read_count = models.PositiveIntegerField(default=0)
+    words_written_count = models.PositiveIntegerField(default=0)
+    entries_made_count = models.PositiveBigIntegerField(default=0)
+    global_rank = models.PositiveBigIntegerField()
 
-class UserStats(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    total_points = models.IntegerField()
-    rank = models.IntegerField()
-    books_read = models.IntegerField()
-    chapters_read = models.IntegerField()
-    words_written = models.IntegerField()
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
-class UserEntries(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    entry = models.TextField()
-    book = models.ForeignKey
+
+
 
--- project/books/models.py
+++ project/books/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+    STATUS_CHOICES = [
+        ("to-read", "To Read"),
+        ("reading", "Currently Reading"),
+        ("read", "Read"),
+    ]
+
+    title = models.CharField(max_length=500)
+    author = models.CharField(max_length=200)
+    cover_image = models.ImageField()
+    page_count = models.PositiveIntegerField()
+    point_potential = models.PositiveIntegerField()
+    reading_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="to-read")
+
+    def __str__(self):
+        return self.title
+
+class ReadingLog(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="entries")
+    percentage_complete = models.PositiveIntegerField()
+    points_earned = models.PositiveIntegerField()
+    # set a min length
+    entry = models.TextField()
+    # automatically sets and is immutable
+    date_of_entry = models.DateField(auto_add_now=True)
+
+    
+
+
+
+
+
