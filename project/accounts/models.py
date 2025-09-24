4b5e4d6125df58c3af7a0d91993664c4dbd7b11b
Benjamin <benjamin630@icloud.com>
2025-09-24 10:20:04 -0700

    solved issue with saving a user entry. was using .filter() instead of .get() to retrieve a book. onto friendships

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -7,17 +7,21 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     # OneToOneField links the user to each instance of the model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(User, blank=True, related_name="friends")
     points = models.PositiveIntegerField(default=0)
     profile_picture = models.ImageField(default="avatar.jpg", upload_to="profile_pictures")
     books_read = models.PositiveIntegerField(default=0)
     words_written = models.PositiveIntegerField(default=0)
     entries_made = models.PositiveIntegerField(default=0)
     pages_read = models.PositiveIntegerField(default=0)
-    global_rank = models.PositiveBigIntegerField(default=0)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name="to_user", on_delete=models.CASCADE)
 
 
 
