bac5a19771bf589cb0be8ad2a7e3d33935edd003
Benjamin <benjamin630@icloud.com>
2025-10-06 14:49:53 -0700

    fixed another thing

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 GOOGLE_BOOKS_API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
 
