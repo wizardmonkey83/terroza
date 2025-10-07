d8579ef9d85c65446f66746b057401e843c8efe4
Benjamin <benjamin630@icloud.com>
2025-10-07 08:26:29 -0700

    migrated to MySQL

--- project/project/settings.py
+++ project/project/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pythonsnatcher18$default', 
+        'USER': 'pythonsnatcher18',   
+        'PASSWORD': config('DB_PASSWORD'),  
+        'HOST': 'pythonsnatcher18.mysql.pythonanywhere-services.com',
     }
 }
 
